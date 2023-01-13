3df9c731697bace78ec8a86d6b66fcda9027faef
seongddiyong <kodd1102@naver.com>
2023-01-13 23:20:08 +0900

    [fix]: b1546

--- BAEKJOON/b1546.cpp
+++ BAEKJOON/b1546.cpp
@@ -13,18 +13,17 @@ int main() {
 
     for(int i=0; i<n; i++) {
         cin >> arr[i];
-        if(arr[i] > 100 || arr[i] < 0) cout << "error";
         if(i == (n-1)) {
             sort(arr, arr + n);
             max = arr[n-1];
         }
-        if(arr[i] == 0) count++;
-        if(count == n-2) break; 
     }
     for(int i=0; i<n; i++){
         arr[i] = arr[i]/max*100;
         sum += arr[i];
-    }
-
+    } 
+    // 이 부분의 for이 딱히 상관 없을 수도 있다.
+    //문제에서는 최댓값은 계산하지 말라는 말은 안했기에
+    // 그대로 묶어서 한 번에 계산하는게 더 빠르다.
     cout << (sum/n);
 }
