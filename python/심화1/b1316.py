568c516a869e9a4fab4562fcda221eb220e8e336
seongddiyong <kodd1102@naver.com>
2023-04-02 00:59:53 +0900

    그룹 단어 체커

---
+++ python/심화1/b1316.py
@@ -0,0 +1,13 @@
+n = int(input())
+count = n
+
+for i in range(n):
+    word = input()
+
+    for j in range(len(word)-1):
+        if word[j] == word[j+1]:
+            continue
+        elif word[j] in word[j+1:]:
+            count -= 1
+            break
+print(count)
