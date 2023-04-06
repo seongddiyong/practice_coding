72979edaa073ebc94acd1ae6c96f41d475924680
seongddiyong <kodd1102@naver.com>
2023-04-07 03:15:40 +0900

    소수

--- python/약수_배수와소수/b2581.py
+++ python/약수_배수와소수/b2581.py
@@ -5,15 +5,14 @@ m = int(input())
 n = int(input())
 result = []
 for i in range(m,n+1):
-    if i == 1:
-        pass
     temp = 0
-    for k in range(2,i):
-        if i%k == 0:
-            temp += 1
-            break
-    if temp == 0:
-        result.append(i)
+    if i > 1:
+        for k in range(2,i):
+            if i%k == 0:
+                temp += 1
+                break
+        if temp == 0:
+            result.append(i)
 if len(result)==0:
     print("-1")
 else:
