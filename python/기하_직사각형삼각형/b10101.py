87e3e9fc710c3f46438dcb0f75cb93d31c5b1fd2
seongddiyong <kodd1102@naver.com>
2023-04-07 18:41:38 +0900

    삼각형 외우

--- python/기하_직사각형삼각형/b10101.py
+++ python/기하_직사각형삼각형/b10101.py
@@ -2,11 +2,11 @@ a = int(input())
 b = int(input())
 c = int(input())
 
-if a == b == c == 60:
+if a == b == c:
     print("Equilateral")
 elif a+b+c != 180:
     print("Error")
-elif a+b+c == 180 and a != b != c:
-    print("Scalene")
-else:
+elif a == b != c or b == c != a or a == c != b:
     print("Isosceles")
+else:
+    print("Scalene")
