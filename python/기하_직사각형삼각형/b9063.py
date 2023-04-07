494b9e744976646daaea2bca626ea5123e12c66b
seongddiyong <kodd1102@naver.com>
2023-04-07 18:30:24 +0900

    대지

---
+++ python/기하_직사각형삼각형/b9063.py
@@ -0,0 +1,13 @@
+from sys import stdin
+input = stdin.readline
+n = int(input())
+
+x = []
+y = []
+for _ in range(n):
+    a,b = map(int,input().split())
+    x.append(a)
+    y.append(b)
+x.sort()
+y.sort()
+print((x[-1]-x[0])*(y[-1]-y[0]))
