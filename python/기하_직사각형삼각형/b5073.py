3086611fe4c9d0253b6cdbcdebfe91df39e858b0
seongddiyong <kodd1102@naver.com>
2023-04-07 18:53:14 +0900

    삼각형과 세 변

---
+++ python/기하_직사각형삼각형/b5073.py
@@ -0,0 +1,12 @@
+while 1:
+    a,b,c = map(int,input().split())
+    if a == b == c == 0:
+        break
+    elif max(a,b,c) >= ((a+b+c)-max(a,b,c)):
+        print("Invaild")
+    elif a==b==c:
+        print("Equilateral")
+    elif a==b!=c or b==c!=a or a==c!=b:
+        print("Isosceles")
+    elif a != b != c:
+        print("Scalene")
