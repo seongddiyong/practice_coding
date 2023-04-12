3631b388867149f229e639449b66b04199bd0771
seongddiyong <kodd1102@naver.com>
2023-04-13 00:25:17 +0900

    영화감독 숌

--- python/브루트포스/b1436.py
+++ python/브루트포스/b1436.py
@@ -7,7 +7,7 @@ six_n = 666
 while True:
     if '666' in str(six_n):
         cnt += 1
-    if cnt == n:
-        print(six_n)
-        break
+        if cnt == n:
+            print(six_n)
+            break
     six_n += 1
