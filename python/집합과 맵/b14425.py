142b1100e6107e42fc68e6a00474a0a8cfab17ab
seongddiyong <kodd1102@naver.com>
2023-04-17 22:25:49 +0900

    문자열 집합

--- python/집합과 맵/b14425.py
+++ python/집합과 맵/b14425.py
@@ -3,9 +3,12 @@ input = stdin.readline
 
 n,m = map(int, input().split())
 count = 0
-n_string = [input() for _ in range(n)]
+n_string = set([input() for _ in range(n)])
 for _ in range(m):
     temp = input()
     if temp in n_string:
         count += 1
 print(count)
+
+# set 함수를 쓰기 전엔 3712ms가 나왔고 쓰니까 140ms가 나왔다.
+# 쓰자..
