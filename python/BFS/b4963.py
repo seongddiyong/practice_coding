b927b18d145822e5795274afb73ac4bf43e0742b
seongddiyong <kodd1102@naver.com>
2023-06-20 23:14:19 +0900

    섬의 개수

--- python/BFS/b4963.py
+++ python/BFS/b4963.py
@@ -1,35 +1,39 @@
 from collections import deque
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10000)
 
 dx = [0,0,-1,1,1,1,-1,-1]
 dy = [1,-1,0,0,1,-1,1,-1]
 
-def dfs(x,y):
-    if x < 0 or y < 0 or x >= h or y >= w:
-        return
-    if graph[x][y] == 1:
-        graph[x][y] = 0
+def bfs(x,y):
+    queue = deque()
+    queue.append([x,y])
+    graph[x][y] = 0
+
+    while queue:
+        x,y = queue.popleft()
+        
         for i in range(8):
             nx = x + dx[i]
             ny = y + dy[i]
-            dfs(nx,ny)
+
+            if 0 <= nx < h and 0 <= ny < w and graph[nx][ny] == 1:
+                graph[nx][ny] = 0
+                queue.append([nx,ny])
 
 while True:
     w,h = map(int, input().split())
     if w == 0 and h == 0:
         break
 
-    graph = []
-    for _ in range(h):
-        graph.append(list(map(int, input().split())))
+    graph = [list(map(int,input().split())) for _ in range(h)]
+    visited = [[False]*w for _ in range(h)]
     
     cnt = 0
     for i in range(h):
         for j in range(w):
             if graph[i][j] == 1:
-                dfs(i,j)
+                bfs(i,j)
                 cnt += 1
 
     print(cnt)
