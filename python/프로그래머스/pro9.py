8af16dd04a4bd25d3d2de915a2b65325bc3eab26
seongddiyong <kodd1102@naver.com>
2023-07-14 01:52:27 +0900

    신규 아이디 추천

---
+++ python/프로그래머스/pro9.py
@@ -0,0 +1,30 @@
+def solution(new_id):
+    new_id = new_id.lower()
+    
+    answer = ''
+    for word in new_id:
+        if word.isalnum() or word in '-_.':
+            answer += word
+    
+    while '..' in answer:
+        answer = answer.replace('..', '.')
+        
+    if answer[0] == '.' and len(answer) > 1:
+        answer = answer[1:]
+    if answer[-1] == '.':
+        answer = answer[:-1]
+    
+    if len(answer) == 0:
+        answer += 'a'
+    
+    if len(answer) >= 16:
+        answer = answer[:15]
+        if answer[-1] == '.':
+            answer = answer[:-1]
+    
+    while len(answer) < 3:
+        answer += answer[-1]
+    
+    return answer
+
+solution("=.=")
