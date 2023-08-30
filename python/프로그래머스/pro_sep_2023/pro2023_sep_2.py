b764e7959aed3e13f4c00bda76d9774068a62ea7
seongddiyong <kodd1102@naver.com>
2023-08-31 00:05:57 +0900

    교점에 별 만들기

--- python/프로그래머스/pro_sep_2023/pro2023_sep_2.py
+++ python/프로그래머스/pro_sep_2023/pro2023_sep_2.py
@@ -1,27 +1,28 @@
-import math
+
 def solution(line):
     answer = []
-    max_x, min_x, max_y, min_y = -99999,99999,-99999,99999
-    for i in range(len(line)-1):
+    for i in range(len(line)):
         A,B,E = line[i]
-        for j in range(i,len(line)):
+        for j in range(i+1,len(line)):
             C,D,F = line[j]
             if A*D - B*C == 0:
                 continue
-            else:
-                x = (B*F - E*D) / (A*D - B*C)
-                y = (E*C - A*F) / (A*D - B*C)
-                if x % 1 != 0 or y % 1 != 0:
-                    continue
-                answer.append([int(x),int(y)])
-                max_x = math.ceil(max(max_x,x))
-                min_x = math.floor(min(min_x,x))
-                max_y = math.ceil(max(max_y,y))
-                min_y = math.floor(min(min_y,y))
-    mapp = ['.' * (max_x - min_x+1)] * (max_y - min_y+1)
-    for i,j in answer:
-        mapp[j-1-min_y][i-1-min_x].replace('.','*')
-        
+            x = (B*F - E*D) / (A*D - B*C)
+            y = (E*C - A*F) / (A*D - B*C)
+            if x % 1 != 0 or y % 1 != 0:
+                continue
+            answer.append([int(x),int(y)])
+            max_x = max(answer)[0]
+            min_x = min(answer)[0]
+            max_y = max(answer, key=lambda x: x[1])[1]
+            min_y = min(answer, key=lambda x: x[1])[1]
+    mapp = [['.'] * (max_x - min_x+1) for _ in range((max_y - min_y+1))]
+    for s in answer:
+        a,b = s
+        x,y = abs(max_y-b) , abs(min_x-a)
+        mapp[x][y] = '*'
+    for i,v in enumerate(mapp):
+        mapp[i] = ''.join(v)
     return mapp
 
 print(solution([[2, -1, 4], [-2, -1, 4], [0, -1, 1], [5, -8, -12], [5, 8, 12]]))
