4a2f28ccc554a4287473a60b2087cbd52148bf56
seongddiyong <kodd1102@naver.com>
2023-09-05 17:45:11 +0900

    영어 끝말잇기

--- python/프로그래머스/pro21.py
+++ python/프로그래머스/pro21.py
@@ -1,9 +1,14 @@
-def solution(d, budget):
-    d = list(sorted(d))
-    while sum(d) > budget:
-        d.pop()
-    return len(d)
-
-d = [2,2,3,3]
-budget = 10
-print(solution(d,budget))
+def solution(n, words):
+    li = [words[0]]
+    cnt1 = 2
+    cnt2 = 1
+    for i,j in enumerate(words[1:]):
+        if j not in li and li[-1][-1] == j[0]:
+            li.append(j)
+            cnt1 += 1
+            if cnt1 > n:
+                cnt1 = 1
+                cnt2 += 1
+        else:
+            return [cnt1,cnt2]
+    return [0,0]
