5ca4f30175e442a9ea773817d048d3abc9d44e84
seongddiyong <kodd1102@naver.com>
2023-09-12 00:41:20 +0900

    신입사원 교육

---
+++ python/프로그래머스/pro_sep_2023/pro2023_sep_13.py
@@ -0,0 +1,16 @@
+import heapq
+def solution(ability, number):    
+    queue = []
+    for a in ability:
+        heapq.heappush(queue, a)
+    
+    for _ in range(number):
+        x = heapq.heappop(queue)
+        y = heapq.heappop(queue)
+        new = x + y
+        heapq.heappush(queue, new)
+        heapq.heappush(queue, new)
+    
+    return sum(queue)
+
+print(solution([10,3,7,2],2))
