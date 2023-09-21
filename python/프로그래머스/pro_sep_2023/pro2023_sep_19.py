a31387e259aa7b57e0c13a8e544b9624c3445bdc
seongddiyong <kodd1102@naver.com>
2023-09-22 00:28:11 +0900

    타겟 넘버 (코드 추가)

--- python/프로그래머스/pro_sep_2023/pro2023_sep_19.py
+++ python/프로그래머스/pro_sep_2023/pro2023_sep_19.py
@@ -16,3 +16,22 @@ def solution(numbers, target):
             if r == target:
                 answer += 1
     return answer
+
+def solution2(numbers, target):
+    n = len(numbers)
+    answer = 0
+    def dfs(idx, result):
+        if idx == n:
+            if result == target:
+                nonlocal answer
+                answer += 1
+            return
+        else:
+            dfs(idx+1, result+numbers[idx])
+            dfs(idx+1, result-numbers[idx])
+    dfs(0,0)
+    return answer
+
+numbers = [1,1,1,1,1]
+target = 3
+print(solution2(numbers, target))
