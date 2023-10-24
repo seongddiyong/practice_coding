0e4f0fe743d6925f9a029289d626168a255cc324
seongddiyong <kodd1102@naver.com>
2023-10-24 16:46:50 +0900

    Hasing-50점

---
+++ python/NOTSOLVE_Class_2/b15829.py
@@ -0,0 +1,10 @@
+from sys import stdin
+from string import ascii_lowercase
+input = stdin.readline
+n = int(input())
+s = input().rstrip()
+d = {}
+for k,i in enumerate(ascii_lowercase):
+	d[i] = k+1
+m = [d[s[i]]*(31**i) for i in range(n)]
+print(sum(m))
