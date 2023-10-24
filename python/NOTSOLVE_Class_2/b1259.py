029c70315f9dd7f5a2422a664a2bc51651efb06a
seongddiyong <kodd1102@naver.com>
2023-10-24 15:48:20 +0900

    팰린드롬 수

---
+++ python/NOTSOLVE_Class_2/b1259.py
@@ -0,0 +1,11 @@
+from sys import stdin
+import math
+input = stdin.readline
+while True:
+    n = input().rstrip()
+    if n == '0':
+        break
+    if n == n[::-1]:
+        print("yes")
+    else:
+        print("no")
