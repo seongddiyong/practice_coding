0fcb49f738e6a7acfe97386a35ac71e0ebb4f684
seongddiyong <kodd1102@naver.com>
2023-11-09 00:30:39 +0900

    튜플

--- python/프로그래머스/pro_nov_2023/pro2023_nov_3.py
+++ python/프로그래머스/pro_nov_2023/pro2023_nov_3.py
@@ -8,3 +8,7 @@ def solution(s):
             if j not in answer:
                 answer.append(j)
     return answer
+
+from collections import Counter
+def solution2(s):
+    return [i[0] for i in Counter(list(map(int,s.replace('{{','').replace('},{',',').replace('}}','').split(',')))).most_common()]
