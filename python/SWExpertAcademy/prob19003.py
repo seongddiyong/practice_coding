fef615fa28aca5b4aaf1e37e2f68ba58e9679ca8
seongddiyong <kodd1102@naver.com>
2023-11-08 20:38:43 +0900

    팰린드롬 문제

--- python/SWExpertAcademy/prob19003.py
+++ python/SWExpertAcademy/prob19003.py
@@ -1,21 +1,20 @@
-from itertools import permutations
 TC = int(input())
 for tc in range(1,TC+1):
     N, M = map(int,input().split())
-    answer = 0
-    palindrome = []
+    answer = cnt = 0
+    palindrome = False
     isNot = []
     for _ in range(N):
         a = input().rstrip()
         if a != a[::-1]:
             isNot.append(a)
         else:
-            palindrome.append(a)
+            palindrome = True
     for _ in range(len(isNot)):
-        temp = isNot.pop(0)
+        temp = isNot.pop()
         if temp[::-1] in isNot:
-            isNot.append(temp)
-    answer = len(isNot)*M
+            cnt += 2
+    answer = cnt*M
     if palindrome:
         answer += M
     print(f'#{tc} {answer}')
